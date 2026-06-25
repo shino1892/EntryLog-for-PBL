ee42a25f834508dacfd1f49f320403b9de933474
shino <24110016@oic-ok.ac.jp>
2026-06-25 11:36:34 +0900

    fix: インポート文解決

--- app/debug/readID.py
+++ app/debug/readID.py
@@ -6,7 +6,7 @@ import os
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from felica import get_felica_idm
+from app.utility.felica import get_felica_idm
 
 if __name__ == '__main__':
     idm = get_felica_idm()
