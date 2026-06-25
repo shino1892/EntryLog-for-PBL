dae54cfffc3e86de69128a95f1a039bf1d3dee6e
shino <24110016@oic-ok.ac.jp>
2026-06-25 11:06:48 +0900

    feat: DB操作ロジック分離

---
+++ app/repository/entry_repository.py
@@ -0,0 +1,15 @@
+def add_entry(conn, idm:str, timestamp):
+    try:
+        sql = "INSERT INTO entry (num,timestamp) VALUES (%s,%s)"
+        with conn.cursor() as cursor:
+            cursor.execute(sql,(idm,timestamp,))
+            conn.commit()
+            if conn.rowcount > 0:
+                return True
+            else:
+                raise RuntimeError("Failed to Entry.")
+    except mysql.connector.IntegrityError:
+        return False
+    except Exception as e:
+        print(f"Database Error (add_entry): {e}")
+        return None
